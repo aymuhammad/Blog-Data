1fd803a0dc8b4023bb2d56df2ada185a1a4b6474
Auwal Yahaya <auwalyah@yahoo.com>
2023-06-05 02:18:55 +0100

    some modifications

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,18 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
 def post_list(request):
-    post = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': post})
+    posts = Post.published.all()
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts': posts})
+
 
 def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'posts': post})
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
