13a3b45194a861debbc6f5849b08ac785a6bb47c
Auwal Yahaya <auwalyah@yahoo.com>
2023-06-07 17:01:33 +0100

    config both local and online email sender

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,3 +124,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# online email sender
+
+# emails server configuration
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'stilleto16@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+# locally sending emails to terminals
+EMAIL_BACKEND = 'django.core.mail.backends.console.EMAILBackend'
